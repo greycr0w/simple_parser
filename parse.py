4e68dac923041b6cd6a9f7fef0ac5acc546739ae
Alexander Badrishvili <alexbant@teiser.gr>
2019-12-13 00:15:26 +0200

    jesus

--- parse.py
+++ parse.py
@@ -1,9 +1,19 @@
-import bs4, requests, json
-
-json_file = open("creds.json")
-data = json.load(json_file)
-print(data['pass'])
+import bs4, requests, json, datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 
-login = requests.post("http://eclass.informatics.teicm.gr/", data={'uname': data['uname'], 'pass': data['pass'], 'submit': data['submit']})
-print(login.headers)
+
+def sign_in():
+    print("Decorated job")
+    json_file = open("creds.json")
+    data = json.load(json_file)
+    login = requests.post("http://eclass.informatics.teicm.gr/", data={'uname': data['uname'], 'pass': data['pass'], 'submit': data['submit']})
+
+    print(login.headers["Set-Cookie"])
+
+
+
+
+scheduler = BlockingScheduler()
+scheduler.add_job(sign_in, 'interval', hours=1, next_run_time=datetime.datetime.now())
+scheduler.start()
